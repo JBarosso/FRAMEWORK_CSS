ea743d02f383dcb32fd1a117cbe5da782c57fda9
JBarosso <104252764+JBarosso@users.noreply.github.com>
2022-04-24 11:27:30 +0200

    Add files via upload
    Création de dossier styles/styles-noPurge et styles/styles-purge
    
    Gulp:
    - gestion sourcemap (pas de sourcemap pour le fichier css purgé)
    - Test d'intégration de ligne SASS
    - Ajout "menu-open" dans whitelist

--- gulpfile.js
+++ gulpfile.js
@@ -6,22 +6,15 @@ const minify = require('gulp-clean-css');
 const rename = require('gulp-rename');
 const purgecss = require('gulp-purgecss');
 
-// Custom extractor for purgeCSS, to avoid stripping classes with `:` prefixes
-class TailwindExtractor {
-  static extract(content) {
-    return content.match(/[A-z0-9-:\/]+/g) || [];
-  }
-}
-
 //? compile, prefix, and min scss
 function scssTask(){
 
   console.log('\x1b[33m%s\x1b[0m', 'Compilation en cours...');
 
-  return src('./styles/style.scss', { sourcemaps: false }) 
+  return src('./styles/styles-noPurge/style.scss', { sourcemaps: true }) 
     .pipe(sass()) //* Compile le fichier sass en css
     .pipe(prefix('last 2 versions')) //* Autoprefixer le fichier css
-    .pipe(dest('./styles')) //* Créer le fichier css
+    .pipe(dest('./styles/styles-noPurge', { sourcemaps: '.' })) //* Créer le fichier css
 
     .on('end', function(){ console.log('\x1b[96m%s\x1b[0m', 'Compilation Done !'); });
 };
@@ -29,12 +22,12 @@ function scssTask(){
 //? Purge css
 function purgeTask(){
 
-  console.log('\x1b[33m%s\x1b[0m', 'Purge css en cours...');
+  console.log('\x1b[31m%s\x1b[0m', 'Purge css en cours...');
 
-  return src('./styles/style.css', { sourcemaps: true }) 
+  return src('./styles/styles-noPurge/style.css', { sourcemaps: false }) 
     .pipe(purgecss({
       content: [
-        './*.html'
+        './pages/*.html'
       ],
       extractors: [
           {
@@ -44,16 +37,18 @@ function purgeTask(){
               extensions: ['css', 'html']
           }
       ],
+      safelist: ['menu-open']
     }))
-    .pipe(dest('./')) //* Créer le fichier css
+    .pipe(minify())
+    .pipe(dest('./styles/styles-purge')) //* Créer le fichier css purgé
 
     .on('end', function(){ console.log('\x1b[96m%s\x1b[0m', 'Purge Done !'); });
 };
 
 //? Watch
 function watchTask(){
-  watch(['./styles/style.scss'], series(scssTask, purgeTask));
-  watch('./*.html', purgeTask);
+  watch(['./styles/styles-noPurge/style.scss'], series(scssTask, purgeTask));
+  watch('./pages/*.html', purgeTask);
 }
 
 //? Default Gulp task 
