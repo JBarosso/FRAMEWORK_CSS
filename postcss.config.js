a752c1d51398252daa5b7f55aca9244ec527af6c
JBarosso <104252764+JBarosso@users.noreply.github.com>
2022-04-24 09:53:58 +0200

    Delete postcss.config.js

--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-const whitelist = /^(\.|p|ul|ol|h\d+|pre|code|carousel|flickity|spinner)/
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: ['./src/**/*.html'],
-
-  whitelistPatterns: [whitelist],
-  whitelistPatternsChildren: [whitelist] 
-});
