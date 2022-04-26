9eca1c3e145ee7efdbaed0b3658bb03ae4f73d7f
JBarosso <104252764+JBarosso@users.noreply.github.com>
2022-04-26 11:51:09 +0200

    Add files via upload
    GULP:
    - purgecss ignore between 2 comments 
    ['/*! purgecss start ignore */', '/*! purgecss end ignore */']
    - purgecss work with multiple html file
    
    SASS
    - add new class for text, font, ect...

--- gulpfile.js
+++ gulpfile.js
@@ -3,23 +3,22 @@ const { src, dest, watch, series} = require('gulp');
 const sass = require('gulp-sass')(require('sass')); 
 const prefix = require('gulp-autoprefixer');
 const minify = require('gulp-clean-css');
-const rename = require('gulp-rename');
 const purgecss = require('gulp-purgecss');
 
-//? compile, prefix, and min scss
+//? COMPILE SCSS, AUTOPREFIXER, CSSMAP
 function scssTask(){
 
   console.log('\x1b[33m%s\x1b[0m', 'Compilation en cours...');
 
   return src('./styles/styles-noPurge/style.scss', { sourcemaps: true }) 
-    .pipe(sass()) //* Compile le fichier sass en css
-    .pipe(prefix('last 2 versions')) //* Autoprefixer le fichier css
-    .pipe(dest('./styles/styles-noPurge', { sourcemaps: '.' })) //* Créer le fichier css
+    .pipe(sass()) //* Compile SCSS
+    .pipe(prefix('last 2 versions')) //* Autoprefixer CSS file
+    .pipe(dest('./styles/styles-noPurge', { sourcemaps: '.' })) //* Create css file
 
     .on('end', function(){ console.log('\x1b[96m%s\x1b[0m', 'Compilation Done !'); });
 };
 
-//? Purge css
+//? PURGECSS, MINIFY CSS
 function purgeTask(){
 
   console.log('\x1b[31m%s\x1b[0m', 'Purge css en cours...');
@@ -37,10 +36,10 @@ function purgeTask(){
               extensions: ['css', 'html']
           }
       ],
-      safelist: ['menu-open']
-    }))
-    .pipe(minify())
-    .pipe(dest('./styles/styles-purge')) //* Créer le fichier css purgé
+      safelist: ['/*! purgecss start ignore */', '/*! purgecss end ignore */']
+    })) //* PURGE css file
+    .pipe(minify()) //* Minify css file
+    .pipe(dest('./styles/styles-purge')) //* Creat purge css file
 
     .on('end', function(){ console.log('\x1b[96m%s\x1b[0m', 'Purge Done !'); });
 };
